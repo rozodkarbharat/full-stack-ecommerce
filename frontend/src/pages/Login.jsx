1d284edf1051137ae393f0cb507d68bc366b25b7
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2023-08-10 21:18:11 +0530

    api link changed for login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
     e.preventDefault();
     dispatch(SigninLoading());
     axios
-      .post("http://localhost:5000/user/login", {
+      .post("https://ecommerce-electronics.onrender.com/user/login", {
         username: email,
         password,
       })
