28fce91b81f2916597fa971caaec0c5580a84a3d
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2023-08-24 21:40:10 +0530

    cart products added in payment

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,32 +1,35 @@
 const express = require("express");
-require("dotenv").config()
+require("dotenv").config();
 const productModel = require("../model/product.model");
-const stripe = require('stripe')(process.env.STRIPE_CODE)
+const stripe = require("stripe")(process.env.STRIPE_CODE);
 const paymentRoute = express.Router();
 
-
-paymentRoute.post('/create-checkout-session', async (req, res) => {
-  
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-        {
-            price_data: {
-              currency: 'inr',
-              product_data: {
-                name: 'T-shirt',
-              },
-              unit_amount: 2000,
-            },
-            quantity: 1,
+paymentRoute.post("/create-checkout-session", async (req, res) => {
+  let data = req.body.data;
+  let line_items = data.map((item) => {
+    return {
+      price_data: {
+        currency: "inr",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          metadata: {
+            id: item.id,
           },
-    ],
-    mode: 'payment',
-    success_url: `${process.env. YOUR_DOMAIN}/checkout-success`,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    success_url: `${process.env.YOUR_DOMAIN}/checkout-success`,
     cancel_url: `${process.env.YOUR_DOMAIN}/checkout-cancel`,
   });
 
-  res.send({url: session.url});
+  res.send({ url: session.url });
 });
-
 
 module.exports = paymentRoute;
